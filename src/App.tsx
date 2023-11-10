fd7ef76ec1b1b9fb48df64b776c5bca7a7328683
Pascal Bienenstein <pbienenstein@posteo.de>
2023-11-10 13:35:02 +0100

    Feierabend

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,27 @@ const App: FC = () => {
       <div className="inputContainer">
         <input type="text"
         placeholder="Kennzeichen"
-      />
+      /><br></br>
       <input type="text"
         placeholder="Fahrer:in"
-      />
+      /><br></br>
       <input type="number"
         placeholder="Kilometerstand Beginn"
-      />
+      /><br></br>
       <input type="number"
         placeholder="Kilometerstand Ende"
-      />
+      /><br></br>
+      {/* Das mit den <br> kann so nicht bleiben I´m very sure! */}
       <input type="text"
         placeholder="Reisezweck"
-      />
+      /><br></br>
       <input type="date"
         placeholder="Datum"
       />
       </div>
-      <button>Hinzufügen</button>
-      <button>Download</button>
+      
+      <button>Hinzufügen</button>  {/* Button der später die eingabe der Liste hinzufügt */}
+      <button>Download</button>    {/* Download Datei als json  */}
     </div>
   </div>
 }
