140c80508e24f32f5d54c045491edc7075123bba
Pascal Bienenstein <pbienenstein@posteo.de>
2023-11-10 13:08:37 +0100

    create input and buttons without function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+const App: FC = () => {
+  
+  return <div className="App">
+    <h1>Fahrtenbuch</h1>
+    <div className="header">
+      <div className="inputContainer">
+        <input type="text"
+        placeholder="Kennzeichen"
+      />
+      <input type="text"
+        placeholder="Fahrer:in"
+      />
+      <input type="number"
+        placeholder="Kilometerstand Beginn"
+      />
+      <input type="number"
+        placeholder="Kilometerstand Ende"
+      />
+      <input type="text"
+        placeholder="Reisezweck"
+      />
+      <input type="date"
+        placeholder="Datum"
+      />
+      </div>
+      <button>Hinzufügen</button>
+      <button>Download</button>
+    </div>
+  </div>
 }
 
 export default App;
