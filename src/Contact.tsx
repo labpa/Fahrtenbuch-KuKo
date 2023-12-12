ac5a6a4bc5cc38bf5238931ee29d1d2bfbd6fd7e
Pascal <pbienenstein@posteo.de>
2023-12-12 17:48:35 +0100

    irgend was mit links und so

---
+++ src/Contact.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import DriverList from "./Components/DriverList";
+import App from "./App";
+
+const Contact: FC  = () => {
+    return(
+        <div>
+
+
+        </div>
+
+    );
+}
+export default Contact;
