5b92b7afee445c3b8edae4d27965c7d2e45b87f2
Pascal <pbienenstein@posteo.de>
2024-10-11 20:59:47 +0200

    mistake

--- src/Api/fahrtApi.ts
+++ src/Api/fahrtApi.ts
@@ -1,5 +1,8 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
+const supabaseApiKey = process.env.REACT_APP_SUPABASE_API_KEY || '';
+
 const getToken = () => {
     const token = localStorage.getItem("access_Token");
     return token ? `Bearer ${token}` : "";
@@ -8,13 +11,13 @@ const getToken = () => {
 const supabaseApiFahrt = createApi({
     reducerPath: "FahrtApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://havsdrwogfkzzlkemcvz.supabase.co',
+        baseUrl: supabaseUrl,
         prepareHeaders: (headers) => {
             const token = getToken();
             if (token) {
                 headers.set('Authorization',token);
             }
-            headers.set('apikey', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhdnNkcndvZ2Zrenpsa2VtY3Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MzEwOTAsImV4cCI6MjAyMDQwNzA5MH0.CHbFOql-glKAKE_J_DENJHCMZFunAfd-COzXK96Yjd8');
+            headers.set('apikey', supabaseApiKey);
             headers.set('Content-Type', 'application/json');
             return headers;
         },
